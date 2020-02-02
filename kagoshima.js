193859fbbe76613b04ed2172e037531ba0f571f1
kevin-garnette <noyau.imperatif@gmail.com>
2020-02-02 17:35:08 +0000

    let's give up vim for sublime text

--- kagoshima.js
+++ kagoshima.js
@@ -1,23 +1,47 @@
-let budget_kagoshima = [
-"billet", 
-"nootropiques", 
-"vitamines", 
-"box", 
-"stock de bouffe",
-"tram", 
-"muscul"];
 
-let incompressible = budget_kagoshima[0];
+// creation du tableau principal, de la vue d'ensemble
 
-class KPB {
+let budget_matrix = [
+"flight ticket",
+"nootropics", 
+"vitamins", 
+"self-storage",
+"food stock",
+"bus pass", 
+"gym pass",
+"misc"];
+
+// creation de la nomenclature des categories du budget sejour, des colonnes du tableau
+
+class budget_category {
   constructor(objet, estimation, type, importance){
-      this.objet = objet;
+      this.objet = object;
       this.estimation = estimation;
       this.type = type;
       this.importance = importance;
+      this.url = url source;
   }
 }
 
-let nootropics = new KPB("nootropiques", "600", "supplements", "incompressible");
+// creation des categories du budget sejour, du contenu des colonnes du tableau
 
-console.log(nootropics);
+let flight ticket = new budget_category("flight ticket", "600", "transportation", "incompressible");
+let racetams = new budget_category("nootropics", "300", "supplements", "primordial");
+let micronutriments = new budget_category("vitamins", "600", "supplements", "primordial");
+let box = new budget_category("self-storage", "600", "logement", "indispensable");
+let pasta = new budget_category("food stock", "600", "alimentation", "necessaire");
+let bus = new budget_category("bus pass", "600", "transportation", "comfort");
+let gym = new budget_category("gym pass", "600", "hygiene", "comfort");
+let misc = new budget_category("misc", "600", "extra", "ideal");
+
+
+*/------------------------- FIN------------------------------------------------------/*
+
+// creation des sous-tableaux
+
+let nootropics = ['pramiracetam','phenylpiracetam','oxiracetam','noopept','theine caffeine','lions mane'];
+
+let micronutriments = ['Choline','Alpha GPC','Omega 3','Multivitamins','ALCAR',
+  'Zinc','CoQ10','NAC','Vitamines D3','Probiotics','ALA','Mg|Ca|K','Melatonin'];
+
+console.log(vitamins);
