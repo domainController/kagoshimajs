5782f9bf50ac5bf36663766f627a8e76255dcf90
kevin-garnette <noyau.imperatif@gmail.com>
2020-02-21 16:44:11 +0000

    holy shit .reduce and .map make everything much more clean git add .git add .

--- arrayOfSmartDrugs.js
+++ arrayOfSmartDrugs.js
@@ -8,8 +8,9 @@ let smartDrugs =
 	  price: 104,
 	  quantity: 200,
 	  monthlyServingsNeed: 16,
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.52,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "intellimeds.es",
 	  url: "https://www.intellimeds.net/pramiracetam/pramiracetam-capsules",
@@ -20,8 +21,9 @@ let smartDrugs =
 	  price: 22,
 	  quantity: 60,
 	  monthlyServingsNeed: 5, 
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.37,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "reachgenius.se",
 	  url: "https://reachgenius.se/en/nootropics/semax.html"
@@ -32,8 +34,9 @@ let smartDrugs =
 	  price: 48,
 	  quantity: 90,
 	  monthlyServingsNeed: 5,
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.53,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "reachgenius.se",
 	  url: "https://reachgenius.se/en/nootropics/phenylpiracetam.html#/35-amount-90_capsules"
@@ -45,7 +48,8 @@ let smartDrugs =
 	  quantity: 100,
 	  monthlyServingsNeed: 5,
 	  costPerServing: null,
-	  monthlyPrice: null,
+	  productLifetime: null,
+	  monthlyCost: 0.44,
 	  percentage: null,
 	  site: "intellimeds.es",
 	  url: "https://www.intellimeds.net/noopept-tablets-20mg"
@@ -56,8 +60,9 @@ let smartDrugs =
 	  price: 19,
 	  quantity: 120,
 	  monthlyServingsNeed: 120,
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.16,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "reachgenius.se",
 	  url: "https://reachgenius.se/en/nootropics/lion-s-mane.html"
@@ -68,8 +73,9 @@ let smartDrugs =
 	  price: 22,
 	  quantity: 100,
 	  monthlyServingsNeed: 3,
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.22,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "intellimeds.es",
 	  url: "https://www.intellimeds.net/l-theanine/l-theanine-tablets"
@@ -80,8 +86,9 @@ let smartDrugs =
 	  price: 20,
 	  quantity: 60,
 	  monthlyServingsNeed: 15, 
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.33,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "iHerb.com",
 	  url: "https://ie.iherb.com/pr/Doctor-s-Best-Natural-Brain-Enhancers-wtih-AlphaSize-and-SerinAid-60-Veggie-Caps/21289"
@@ -92,8 +99,9 @@ let smartDrugs =
 	  price: 17,
 	  quantity: 180,
 	  monthlyServingsNeed: 15, 
-	  costPerServing: null,
-	  monthlyPrice: null,
+	  costPerServing: 0.09,
+	  productLifetime: null,
+	  monthlyCost: null,
 	  percentage: null,
 	  site: "amazon.co.uk",
 	  url: "https://www.amazon.co.uk/gp/product/B013CRN3S8"
@@ -104,33 +112,59 @@ let smartDrugs =
       price: 10,
       quantity: 180,
       monthlyServingsNeed: 60,
-      costPerServing: null,
-      monthlyPrice: null,
+      costPerServing: 0.06,
+      productLifetime: null,
+      monthlyCost: null,
       percentage: null,
       site: "amazon.com",
       url: "https://www.amazon.com/gp/product/B0019LTGC2/ref=ox_sc_act_title_1?smid=A249CL9PVB6VZW&psc=1"
     }
 ]
 
+// Using .map 
 
-// costPerServing = price / quantity
+	// costPerServing calcul = price / quantity
 
-var costPerServings = smartDrugs.map( function(costPerServing) {
-	return costPerServing.price / costPerServing.quantity
-});
+	var costPerServing = smartDrugs.map(function(supplement) {
+		return ((supplement.price / supplement.quantity).toFixed(2))
+	});
 
-var costPerServing = smartDrugs.map( price => smartDrugs.price / smartDrugs.quantity); 
-console.table(costPerServings);
+	var costPerServing = smartDrugs.map(supplement => ((supplement.price / supplement.quantity).toFixed(2)));
+	console.table(costPerServing);
 
-var costPerServing = smartDrugs.map((prixDeRevient) => {
-console.log(smartDrugs.price)
-}
+// monthlyCost calcul = costPerServing * monthlyServingsNeed: This nOT wORKING ! WHY ?! (21/02)
 
-console.table(costPerServings);
+	var monthlyCost = smartDrugs.map(function(product) {
+		return product.costPerServing * product.monthlyServingsNeed 
+	});
 
-/*
-let new_array = arr.map(function callback( currentValue[, index[, array]]) {
-    // return element for new_array
-}[, thisArg])
-/*
+	console.table(monthlyCost);
 
+// product.lifetime calcul: see moment.js coz 250 j = 8 months 7 days or 6 days or 5 days
+
+	// this gives an integer of days but doesn't translate into "months" anything
+
+	var lifetime = smartDrugs.map(box => (((box.monthlyServingsNeed * box.quantity)/30).toFixed(2)))
+	console.table(lifetime);
+
+// Using .reduce
+
+	// percentage calcul
+
+		// sum calcul : 
+
+		var ArrayTotalSumPrice = smartDrugs.reduce((total, article) => total + article.price, 0);
+		console.log(ArrayTotalSumPrice);
+
+		// old way 
+
+	     var somme = 0;
+	     for (var x = 0; x < smartDrugs.length; x++){
+	       somme += smartDrugs[x].price; }
+	       console.log("la somme totale des supplements est de: " + somme +"€"); 
+
+
+ 	var percentage = smartDrugs.map(function(cognitiveEhancer) {
+ 		return (((cognitiveEhancer.price / ArrayTotalSumPrice)*100).toFixed(1))
+ 	});
+ 	console.table(percentage);
