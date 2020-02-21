fe424601c9fc61f5122bcd7f6fe13944018ba1eb
kevin-garnette <noyau.imperatif@gmail.com>
2020-02-21 18:34:44 +0000

    created dynamic table window onload from array of nootropics

---
+++ tableDraft.js
@@ -0,0 +1,145 @@
+
+    let sortDirection = false;
+	let smartDrugs = 
+[
+	{
+	  description: "pramiracetam",
+	  category: "nootropics",
+	  price: 104,
+	  quantity: 200,
+	  monthlyServingsNeed: 16,
+	  costPerServing: 0.52,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "intellimeds.es",
+	  url: "https://www.intellimeds.net/pramiracetam/pramiracetam-capsules",
+	},
+	{
+	  description: "semax",
+	  category: "nootropics",
+	  price: 22,
+	  quantity: 60,
+	  monthlyServingsNeed: 5, 
+	  costPerServing: 0.37,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "reachgenius.se",
+	  url: "https://reachgenius.se/en/nootropics/semax.html"
+	},
+	{
+	  description: "phenylpiracetam",
+	  category: "nootropics",
+	  price: 48,
+	  quantity: 90,
+	  monthlyServingsNeed: 5,
+	  costPerServing: 0.53,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "reachgenius.se",
+	  url: "https://reachgenius.se/en/nootropics/phenylpiracetam.html#/35-amount-90_capsules"
+	},	
+	{
+	  description: "noopept",
+	  category: "nootropics",
+	  price: 44,
+	  quantity: 100,
+	  monthlyServingsNeed: 5,
+	  costPerServing: null,
+	  productLifetime: null,
+	  monthlyCost: 0.44,
+	  percentage: null,
+	  site: "intellimeds.es",
+	  url: "https://www.intellimeds.net/noopept-tablets-20mg"
+	},
+	{
+	  description: "lion's mane",
+	  category: "stress",
+	  price: 19,
+	  quantity: 120,
+	  monthlyServingsNeed: 120,
+	  costPerServing: 0.16,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "reachgenius.se",
+	  url: "https://reachgenius.se/en/nootropics/lion-s-mane.html"
+	},	
+	{
+	  description: "theine",
+	  category: "stress",
+	  price: 22,
+	  quantity: 100,
+	  monthlyServingsNeed: 3,
+	  costPerServing: 0.22,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "intellimeds.es",
+	  url: "https://www.intellimeds.net/l-theanine/l-theanine-tablets"
+	},
+	{
+	  description: "alpha-gpc",
+	  category: "cholinergics",
+	  price: 20,
+	  quantity: 60,
+	  monthlyServingsNeed: 15, 
+	  costPerServing: 0.33,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "iHerb.com",
+	  url: "https://ie.iherb.com/pr/Doctor-s-Best-Natural-Brain-Enhancers-wtih-AlphaSize-and-SerinAid-60-Veggie-Caps/21289"
+	},
+	{
+	  description: "choline bitartre",
+	  category: "cholinergics",
+	  price: 17,
+	  quantity: 180,
+	  monthlyServingsNeed: 15, 
+	  costPerServing: 0.09,
+	  productLifetime: null,
+	  monthlyCost: null,
+	  percentage: null,
+	  site: "amazon.co.uk",
+	  url: "https://www.amazon.co.uk/gp/product/B013CRN3S8"
+	},
+    {
+      description: "melatonin",
+      category: "sleep",
+      price: 10,
+      quantity: 180,
+      monthlyServingsNeed: 60,
+      costPerServing: 0.06,
+      productLifetime: null,
+      monthlyCost: null,
+      percentage: null,
+      site: "amazon.com",
+      url: "https://www.amazon.com/gp/product/B0019LTGC2/ref=ox_sc_act_title_1?smid=A249CL9PVB6VZW&psc=1"
+    }
+];
+
+	window.onload = () => {
+		loadTableData(smartDrugs);
+	};
+
+    function loadTableData(smartDrugs){
+	const tableBody = document.getElementById('smartDrugs');
+	let dataHtml = '';
+
+	for(let order of smartDrugs) {
+		dataHtml += `<tr><td>${order.description}</td><td>${order.category}</td><td>${order.price}</td>
+		<td>${order.quantity}</td><td>${order.monthlyServingsNeed}</td><td>${order.costPerServing}</td>
+		<td>${order.productLifetime}</td><td>${order.monthlyCost}</td><td>${order.percentage}</td>
+		<td>${order.site}</td><td>${order.url}</td>`
+	}
+
+	console.log(dataHtml);
+	tableBody.innerHTML = dataHtml;
+}
+
+
+// YouTube video Build a Dynamic Table with Sorting | HTML, CSS & JavaScript Frontend Mini Projects | Dylan Israel
+// YouTube video https://www.youtube.com/watch?v=ri5Nqe_IK50
